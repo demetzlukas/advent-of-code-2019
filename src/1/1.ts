d1c219996cb0e1325710e914ae4fe365c075282e
Lukas Demetz <lukas.demetz@fh-kufstein.ac.at>
2019-12-04 13:29:53 +0100

    Nicer solution for day 1

--- src/1/1.ts
+++ src/1/1.ts
@@ -1,28 +1,29 @@
 import { readLinesFromInput } from '../utils/readFile';
 
 const fileName = './input/1.txt';
-const { readFile } = require('fs').promises;
 
-function fuelCalculator(mass: number): number {
+function calculate(mass: number): number {
     return Math.floor(mass / 3) - 2;
 }
 
-function fuelCalculatorWithFuel(mass: number): number {
-    if (fuelCalculator(mass) < 0) return 0;
+function calculateWithFuelMass(mass: number): number {
+    if (calculate(mass) < 0) return 0;
 
-    let fuel = fuelCalculator(mass);
-    return fuel + fuelCalculatorWithFuel(fuel);
+    let fuel = calculate(mass);
+    return fuel + calculateWithFuelMass(fuel);
+}
+
+function calculateFuel(masses: string[], recursive: boolean): number {
+    let calculation = recursive ? calculateWithFuelMass : calculate;
+
+    return masses
+        .map((mass: string) => calculation(parseInt(mass)))
+        .reduce((sum: number, fuel: number) => sum + fuel);
 }
 
 export async function main() {
-    let lines = await readLinesFromInput(fileName);
-    let totalFuel = lines
-        .map((line: string) => fuelCalculator(parseInt(line)))
-        .reduce((sum: number, fuel: number) => sum + fuel);
-    console.log(`Part 1: ${totalFuel}`);
+    let masses = await readLinesFromInput(fileName);
 
-    let totalFuelWithFuel = lines
-        .map((line: string) => fuelCalculatorWithFuel(parseInt(line)))
-        .reduce((sum: number, fuel: number) => sum + fuel);
-    console.log(`Part 2: ${totalFuelWithFuel}`);
+    console.log(`Part 1: ${calculateFuel(masses, false)}`);
+    console.log(calculateFuel(masses, true));
 }
